ca4d830ced3cd02dcaaf73ae0fe947ac35c5aaa0
human-sketch <73455299+human-sketch@users.noreply.github.com>
2021-03-05 16:12:30 +1300

    5th change

--- My Game/Assets/PlayerMovement.cs
+++ My Game/Assets/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D rb;
 
+    public Animator animator;
+
     Vector2 movement;
 
     // Update is called once per frame
@@ -16,10 +18,15 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        
     }
 }
